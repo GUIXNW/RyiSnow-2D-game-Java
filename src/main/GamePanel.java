f57ef940058ee6f7958bf7085dea5b89e21240e8
Claire Tong <jupiteryao@icloud.com>
2023-04-21 16:57:45 -0700

    UI

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -33,9 +33,11 @@ public class GamePanel extends JPanel implements Runnable { // this class inheri
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-    Sound sound = new Sound();
+    Sound music = new Sound();
+    Sound se = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
     Thread gameThread; // start and stop
 
     // ENTITY AND OBJECT
@@ -145,21 +147,24 @@ public class GamePanel extends JPanel implements Runnable { // this class inheri
         // PLAYER
         player.draw(g2);
 
+        // UI
+        ui.draw(g2);
+
         g2.dispose();
     }
 
     public void playMusic(int i) {
-        sound.setFile(i);
-        sound.play();
-        sound.loop();
+        music.setFile(i);
+        music.play();
+        music.loop();
     }
 
     public void stopMusic() {
-        sound.stop();
+        music.stop();
     }
 
     public void playSE(int i) { // sound effect (short)
-        sound.setFile(i);
-        sound.play();
+        se.setFile(i);
+        se.play();
     }
 }
---
+++ src/main/UI.java
@@ -0,0 +1,100 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
+
+import object.OBJ_Key;
+
+public class UI {
+    GamePanel gp;
+    Font arial_40, arial_80B;
+    BufferedImage keyImage;
+
+    public boolean messageOn = false;
+    public String message = "";
+    int messageCounter = 0;
+
+    public boolean gameFinished = false;
+
+    // Play time
+    double playTime;
+    DecimalFormat dFormat = new DecimalFormat("#0.00");
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+
+        arial_40 = new Font("Arial", Font.PLAIN, 40);
+        arial_80B = new Font("Arial", Font.BOLD, 80);
+
+        OBJ_Key key = new OBJ_Key();
+        keyImage = key.image;
+    }
+
+    public void showMessage(String text) {
+        message = text;
+        messageOn = true;
+    }
+
+    public void draw(Graphics2D g2) {
+        if (gameFinished) {
+
+            g2.setFont(arial_40);
+            g2.setColor(Color.white);
+
+            String text;
+            int textLength;
+            int x, y;
+
+            text = "You found the treasure!";
+            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth(); // get the length of text
+            x = gp.screenWidth/2 - textLength/2;
+            y = gp.screenHeight/2 - (gp.tileSize*3);
+            g2.drawString(text, x, y);
+
+            // Display ending time
+            text = "Your time is :" + dFormat.format(playTime) + "!";
+            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth(); // get the length of text
+            x = gp.screenWidth/2 - textLength/2;
+            y = gp.screenHeight/2 + (gp.tileSize*4);
+            g2.drawString(text, x, y);
+
+            g2.setFont(arial_80B);
+            g2.setColor(Color.yellow);
+            text = "Congratulations!";
+            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth(); // get the length of text
+            x = gp.screenWidth/2 - textLength/2;
+            y = gp.screenHeight/2 + (gp.tileSize*2);
+            g2.drawString(text, x, y);
+
+            gp.gameThread = null; // stop the thread
+        }
+        else {
+            g2.setFont(arial_40);
+            g2.setColor(Color.white);
+            g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
+            g2.drawString("x " + gp.player.hasKey, 74, 65);
+
+            // TIME
+            playTime += (double)1/60;
+            g2.drawString("Time: "+ dFormat.format(playTime), gp.tileSize*11, 65);
+
+            // MESSAGE
+            if (messageOn) {
+                g2.setFont(g2.getFont().deriveFont(30F));
+                g2.drawString(message, gp.tileSize/2, gp.tileSize*5);
+
+                messageCounter++;
+
+                if (messageCounter > 120) { // 2 seconds 120 frames
+                    messageCounter = 0;
+                    messageOn = false;
+                }
+            }
+        }
+
+        
+    }
+}
